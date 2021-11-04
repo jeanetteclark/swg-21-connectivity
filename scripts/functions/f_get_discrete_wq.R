2a742acc4184223726c1133591eeedea5beac714
Jeanette Clark <jclark@nceas.ucsb.edu>
2021-11-04 15:23:39 -0700

    drop pins

--- scripts/functions/f_get_discrete_wq.R
+++ scripts/functions/f_get_discrete_wq.R
@@ -1,11 +1,9 @@
 library(readr)
 library(janitor)
-library(pins)
 
 f_get_discrete_wq <- function(){
   source_url <- "https://portal.edirepository.org/nis/dataviewer?packageid=edi.731.1&entityid=6c5f35b1d316e39c8de0bfadfb3c9692"
 
-  pin(source_url)
   wq <- source_url  %>%
     read_csv(show_col_types = FALSE) %>%
     clean_names()
